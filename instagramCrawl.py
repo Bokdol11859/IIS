449f23060f02961d2cd0510124abedb79d0d2de9
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-05-19 16:51:19 +0900

    Update instagramCrawl.py

--- instagramCrawl.py
+++ instagramCrawl.py
@@ -26,9 +26,6 @@ driver_path = '.\chromedriver.exe'
 
 
 options = webdriver.ChromeOptions()
-# options.add_argument('--no-sandbox')
-# options.add_argument('--disable-dev-shm-usage')
-# options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
 
 driver = webdriver.Chrome(driver_path,options=options)
 
@@ -38,7 +35,7 @@ except:
     print("Main folder already exists")
 
 time.sleep(2)
-## url에 접근한다.
+
 driver.get('https://www.instagram.com')
 time.sleep(1)
 driver.find_element_by_name("username").send_keys(username)
@@ -47,6 +44,7 @@ driver.find_element_by_name("password").send_keys(password)
 time.sleep(2)
 driver.find_element_by_xpath("//*[@id=\"loginForm\"]/div/div[3]/button").click()
 time.sleep(3)
+
 for targ in target:
     driver.get(f'https://www.instagram.com/{targ}/')
     last_height = driver.execute_script("return document.body.scrollHeight")
@@ -69,13 +67,14 @@ for targ in target:
         imgs = bs.find_all('img', {'class': 'FFVAD'})
         for i in imgs:
             if i['src'] not in images:
-                # print(i['src'])
                 images.append(i['src'])
                 urllib.request.urlretrieve(i['src'], f'./instaImages/{targ}/{count}.jpg')
                 count += 1
+                
     html = driver.page_source
     bs = BeautifulSoup(html, 'html.parser')
     imgs = bs.find_all('img', {'class': 'FFVAD'})
+    
     for i in imgs:
         if i['src'] not in images:
             # print(i['src'])
